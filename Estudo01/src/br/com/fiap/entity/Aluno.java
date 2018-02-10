bdd633617f7796d7cd933282df11718c6e0ad88a
Attos Lins <attoslins@icloud.com>
2018-02-10 14:59:07 -0200

    testando segundo commit

--- Estudo01/src/br/com/fiap/entity/Aluno.java
+++ Estudo01/src/br/com/fiap/entity/Aluno.java
@@ -1,5 +1,24 @@
 package br.com.fiap.entity;
 
 public class Aluno {
+	private String nome;
 
+	public Aluno(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Aluno() {
+		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
